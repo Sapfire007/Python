d69afc071a463233630b1b0b2f72d9f062ef997b
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-06-29 19:52:27 +0000

    commit for Day002

---
+++ Day002/main.py
@@ -0,0 +1,23 @@
+'''
+This is how multi-line comments are written in python
+insted of using # symbol
+'''
+"""This
+can 
+also
+be
+done"""
+print("Hello World, this is Saptarshi learning python\nand this is my 2nd programming language after JAVA ")
+# \n is used to print in next line also called escape sequence character
+print("This is a \"double quote\" done by \"double qoute escape sequance character.\"")
+# \" is used to print double quote in output
+print('He said, "I want to eat an apple".')
+# print('This is a \'single quote\'' done by \'single qoute escape sequance character.\'')
+# \' is used to print single quote in output
+print('This is the use of \'single quotation\' ')
+print("Sapfire",0,0,7)
+print("Sapfire",0,0,7,sep="~")
+print("Sapfire",0,0,7,sep="")
+print("Sapfire",777)
+print("Sapfire",0,0,7,sep="~",end="004\n")
+print("Sapfire")
