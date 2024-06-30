7290a259e352650c6944dcbd7b167faadad4106c
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-06-30 06:58:29 +0000

    initial commit

---
+++ Day003/variables.py
@@ -0,0 +1,18 @@
+a = 7
+b = 1.25
+c = "Saptarshi"
+d = True
+e = None
+f = complex(8, 2)
+print("The value of a is: ",a,", of type: ",type(a))
+print("The value of b is: ",b,", of type: ",type(b))
+print("The value of c is: ",c,", of type: ",type(c))
+print("The value of d is: ",d,", of type: ",type(d))
+print("The value of e is: ",e,", of type: ",type(e))
+print("The value of f is: ",f,", of type: ",type(f))
+list = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print("The value of list is: ",list,", of type: ",type(list))
+tuple = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print("The value of tuple is: ",tuple,", of type: ",type(tuple))
+dict = {"name":"Saptarshi", "age":18, "canVote":True}
+print("The value of dict is: ",dict,", of type: ",type(dict))
