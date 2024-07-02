35ae9024cdd87c71526cc04fe49b1450bfe4d3ec
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-07-02 20:31:45 +0000

    initial commit

---
+++ Day012/main.py
@@ -0,0 +1,24 @@
+name = "Saptarshi"
+for i in name:
+  print(i)
+  if (i == "p"):
+    print("The \"p\" in my name stands for POWER")
+
+for i in name:
+  print(i, end=", ")
+
+fruits = ["Mango", "Apple", "Cherry", "Guava"]
+for x in fruits:
+  print("\n",x)
+  for y in x:
+    print(y)
+
+print("\n")
+for z in range(8):
+  print(z)
+print("\n")
+for z in range(9,17):
+  print(z+1)
+print("\n")
+for z in range(0,15,3):
+  print(z)
