22a3b8409c69a234da3dd9cc57cb6490bb71e380
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-07-07 05:43:36 +0000

    initial commit

---
+++ Day025/main.py
@@ -0,0 +1,11 @@
+s = {2,5,8,6,2,4,8,7,3,2} #set
+print(type(s))
+print(s)
+t = {"Saptarshi",7,True,17.5,True,7}   #Randomised order is generated in output; thus cant be accessed by index number
+print(t)
+for items in t:
+  print(items)
+emptyd = {}  #this becomes an empty dictionary
+print(type(emptyd))
+emptys = set() #this is now an empty set
+print(type(emptys))
