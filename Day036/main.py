0090d5e147b8f0e8f166fc182c34f41e51b99895
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-07-12 08:32:06 +0000

    initial commit

--- Day036/main.py
+++ Day036/main.py
@@ -1,3 +1,5 @@
+# Virtual Environment in Python
+
 """
 Virtual Environment
 A virtual environment is a tool used to isolate specific Python environments on a single machine, allowing you to work on multiple projects with different dependencies and packages without conflicts. This can be especially useful when working on projects that have conflicting package versions or packages that are not compatible with each other.
