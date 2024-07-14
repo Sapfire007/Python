8c54d1f179016bb81c95bede3ba7986ec20ca188
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-07-14 15:19:46 +0000

    initial commit

---
+++ Day038/Saptarshi.py
@@ -0,0 +1,8 @@
+def wlc():
+  print("Hey welcome to my repository")
+  # print(__name__) #It'll print in main.py
+
+print(__name__) #It'll print in main.py
+if __name__ == "__main__":
+  wlc()
+  print(__name__) #It'll not print in main.py
---
+++ Day038/main.py
@@ -0,0 +1,4 @@
+import Saptarshi
+
+# print(__name__)
+Saptarshi.wlc()
